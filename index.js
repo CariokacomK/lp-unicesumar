4a76674281ac9f0af586b847723484cd413f953b
Gabriel Borges <gobcarioka@gmail.com>
2023-08-11 16:09:10 -0300

    update functions

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const tipoAcao = document.querySelector('.tipo-acao')
-const nomeAcao = document.querySelector('.nome-acao')
+//APARECER CURSOS DE ACORDO COM OPÇÃO DO RADIO
+const unidadeRadios = document.getElementsByName('unidade');
+const cursos = document.querySelectorAll('.cursos');
 
-tipoAcao.addEventListener('changed', function () {
-    nomeAcao.classList.add('aparecer')
-    console.log();
+for (let i = 0; i < unidadeRadios.length; i++) {
+    unidadeRadios[i].addEventListener('change', () => {
+        for (let j = 0; j < cursos.length; j++) {
+            cursos[j].classList.remove('aparecer');
+        }
+
+        if (unidadeRadios[i].checked) {
+            cursos[i].classList.add('aparecer');
+        }
+    });
+}
+
+// APARECER DIV NOME DA AÇÃO
+const tipoAcao = document.getElementsByTagName('select')
+const nomeAcao = document.getElementsByClassName('nome-acao')
+
+tipoAcao[6].addEventListener('change', () => {
+    nomeAcao[0].classList.add('aparecer')
 })
+
+
+
 
 
 
